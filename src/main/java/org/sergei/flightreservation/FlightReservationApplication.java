e846062ed7b107a48515970fa56053b53a3b0275
Sergei <sergei.visotsky@gmail.com>
2018-10-21 11:41:37 +0300

    Initial commit

---
+++ src/main/java/org/sergei/flightreservation/FlightReservationApplication.java
@@ -0,0 +1,24 @@
+package org.sergei.flightreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class FlightReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightReservationApplication.class, args);
+    }
+
+    @Controller
+    class WelcomeEntry {
+        @GetMapping
+        @ResponseBody
+        public String welcome() {
+            return "Flight reservation app";
+        }
+    }
+}
