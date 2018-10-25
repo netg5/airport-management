fbdf1cbc41a0d0179fd17551bf9e5f8bcffdfefd
Sergei <sergei.visotsky@gmail.com>
2018-10-25 23:09:59 +0300

    Spring security implementation started

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,17 @@
-INSERT INTO flight_reservation.route(arrival_time, departure_time, distance, place, price) VALUES('2018-10-25', 2500, 'New York', 500)
+DROP DATABASE IF EXISTS flight_reservation_oauth;
+CREATE DATABASE flight_reservation_oauth /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+
+DROP TABLE IF EXISTS flight_reservation_oauth.oauth_client_details;
+CREATE TABLE flight_reservation_oauth.oauth_client_details (
+  client_id               VARCHAR(256) PRIMARY KEY,
+  resource_ids            VARCHAR(256),
+  client_secret           VARCHAR(256),
+  scope                   VARCHAR(256),
+  authorized_grant_types  VARCHAR(256),
+  web_server_redirect_uri VARCHAR(256),
+  authorities             VARCHAR(256),
+  access_token_validity   INTEGER,
+  refresh_token_validity  INTEGER,
+  additional_information  VARCHAR(4096),
+  autoapprove             VARCHAR(256)
+);
