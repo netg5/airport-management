9af1dbf08dc167bfc13457dc0d055106141c845a
Sergei <sergei.visotsky@gmail.com>
2018-11-04 18:32:12 +0200

    List with route reservations removed from the customer DTO

--- src/main/java/org/sergei/flightreservation/dto/CustomerDTO.java
+++ src/main/java/org/sergei/flightreservation/dto/CustomerDTO.java
@@ -18,17 +18,14 @@ public class CustomerDTO {
     private String lastName;
     private Integer age;
 
-    private List<FlightReservation> flightReservationList = new LinkedList<>();
-
     public CustomerDTO() {
     }
 
-    public CustomerDTO(Long customerId, String firstName, String lastName, Integer age, List<FlightReservation> flightReservationList) {
+    public CustomerDTO(Long customerId, String firstName, String lastName, Integer age) {
         this.customerId = customerId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
-        this.flightReservationList = flightReservationList;
     }
 
     public Long getCustomerId() {
@@ -61,13 +58,5 @@ public class CustomerDTO {
 
     public void setAge(Integer age) {
         this.age = age;
-    }
-
-    public List<FlightReservation> getFlightReservationList() {
-        return flightReservationList;
-    }
-
-    public void setFlightReservationList(List<FlightReservation> flightReservationList) {
-        this.flightReservationList = flightReservationList;
     }
 }
