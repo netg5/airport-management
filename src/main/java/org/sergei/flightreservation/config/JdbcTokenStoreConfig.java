ea1e4f34c080ad370c812e0458f59bc17a74832a
Sergei <sergei.visotsky@gmail.com>
2018-11-05 14:54:07 +0200

    Comments added

--- src/main/java/org/sergei/flightreservation/config/JdbcTokenStoreConfig.java
+++ src/main/java/org/sergei/flightreservation/config/JdbcTokenStoreConfig.java
@@ -3,7 +3,6 @@ package org.sergei.flightreservation.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
