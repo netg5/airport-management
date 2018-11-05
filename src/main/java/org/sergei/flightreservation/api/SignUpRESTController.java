fe360dfad8ed3488f189f92dad2d05d937dbe461
Sergei <sergei.visotsky@gmail.com>
2018-11-05 20:38:05 +0200

    Signup REST controller set to be ignored

--- src/main/java/org/sergei/flightreservation/api/SignUpRESTController.java
+++ src/main/java/org/sergei/flightreservation/api/SignUpRESTController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.Collections;
 
 /**
  * @author Sergei Visotsky, 2018
  */
+@ApiIgnore
 @RestController
 @RequestMapping("/signup")
 public class SignUpRESTController {
