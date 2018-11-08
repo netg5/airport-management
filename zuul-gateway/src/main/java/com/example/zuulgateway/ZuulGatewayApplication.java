b314754f74f226b2bc195baa975f6ac10269dc2b
Sergei <sergei.visotsky@gmail.com>
2018-11-08 14:42:49 +0200

    Eureka and zuul updated

--- zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
@@ -1,12 +1,10 @@
-package org.sergei.zuulgateway;
+package com.example.zuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 @EnableDiscoveryClient
 public class ZuulGatewayApplication {
 
--- zuul-gateway/src/test/java/com/example/zuulgateway/ZuulGatewayApplicationTests.java
+++ zuul-gateway/src/test/java/com/example/zuulgateway/ZuulGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package org.sergei.zuulgateway;
+package com.example.zuulgateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
