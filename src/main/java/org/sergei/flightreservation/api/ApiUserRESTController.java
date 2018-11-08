cab3f2e2907d1bbd6c86bb81eae7ff1c4b90c6ce
Sergei <sergei.visotsky@gmail.com>
2018-11-08 08:31:58 +0200

    .gitignore update

--- src/main/java/org/sergei/flightreservation/api/ApiUserRESTController.java
+++ src/main/java/org/sergei/flightreservation/api/ApiUserRESTController.java
@@ -6,26 +6,31 @@ import org.sergei.flightreservation.service.SignUpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Sergei Visotsky, 2018
  */
 @ApiIgnore
 @RestController
-@RequestMapping("/signup")
+@RequestMapping(value = "/users", produces = "application/json")
 public class SignUpRESTController {
 
     @Autowired
     private SignUpService signUpService;
 
-    @PostMapping
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('ROLE ADMIN')")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(signUpService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = "application/json")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         user.setUserRoles(Collections.singletonList(new UserRoles("USER")));
         User newUser = signUpService.saveUser(user);
--- src/main/java/org/sergei/flightreservation/service/ApiUserService.java
+++ src/main/java/org/sergei/flightreservation/service/ApiUserService.java
@@ -9,6 +9,9 @@ import org.sergei.flightreservation.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * @author Sergei Visotsky, 2018
@@ -23,6 +26,10 @@ public class SignUpService {
     public SignUpService(UserDAO userDAO, PasswordEncoder passwordEncoder) {
         this.userDAO = userDAO;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    public List<User> getAllUsers() {
+        return userDAO.findAll();
     }
 
     public User saveUser(User user) {
