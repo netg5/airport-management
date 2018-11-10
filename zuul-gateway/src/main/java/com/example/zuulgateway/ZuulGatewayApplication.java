504092defce0fdf6705bfaa8639c75f1f6f82662
Sergei <sergei.visotsky@gmail.com>
2018-11-10 21:39:33 +0200

    Dpendency cleanup and config update

--- zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +15,14 @@ public class ZuulGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+    @Controller
+    class WelcomeEntry {
+        @GetMapping("/")
+        @ResponseBody
+        public String welcome() {
+            return "Zuul Proxy";
+        }
     }
 }
