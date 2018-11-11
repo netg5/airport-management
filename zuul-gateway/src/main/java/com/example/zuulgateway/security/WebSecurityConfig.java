d6a322e5ac8eca987d4f448e71945a1129fee1fb
Sergei <sergei.visotsky@gmail.com>
2018-11-11 14:23:18 +0200

    WebSecurityConfig added

--- zuul-gateway/src/main/java/com/example/zuulgateway/security/WebSecurityConfig.java
+++ zuul-gateway/src/main/java/com/example/zuulgateway/security/WebSecurityConfig.java
@@ -2,107 +2,34 @@
  * Copyright (c) 2018 Sergei Visotsky
  */
 
-package org.sergei.authserver.security;
+package com.example.zuulgateway.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
-import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
-import org.springframework.security.oauth2.provider.approval.TokenStoreUserApprovalHandler;
-import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationProcessingFilter;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 /**
  * @author Sergei Visotsky, 2018
  */
 @Configuration
-@EnableWebSecurity
-@EnableOAuth2Client
+@EnableOAuth2Sso
+@EnableResourceServer
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final ClientDetailsService clientDetailsService;
-    private final ApiUserDetailsService apiUserDetailsService;
-
     @Autowired
-    public WebSecurityConfig(ClientDetailsService clientDetailsService,
-                             ApiUserDetailsService apiUserDetailsService) {
-        this.clientDetailsService = clientDetailsService;
-        this.apiUserDetailsService = apiUserDetailsService;
-    }
-
-    // All users are stored into the database
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(apiUserDetailsService).passwordEncoder(encoder());
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    // In case of JWT TokenStore
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(jwtTokenEnhancer());
-    }
-
-    @Bean
-    protected JwtAccessTokenConverter jwtTokenEnhancer() {
-        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("jwt.jks"), "secretKey".toCharArray());
-
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("jwt"));
-
-        return converter;
-    }
-
-    @Bean
-    @Autowired
-    public TokenStoreUserApprovalHandler userApprovalHandler(TokenStore tokenStore) {
-        TokenStoreUserApprovalHandler handler = new TokenStoreUserApprovalHandler();
-        handler.setTokenStore(tokenStore);
-        handler.setRequestFactory(new DefaultOAuth2RequestFactory(clientDetailsService));
-        handler.setClientDetailsService(clientDetailsService);
-        return handler;
-    }
-
-    @Bean
-    @Autowired
-    public ApprovalStore approvalStore(TokenStore tokenStore) throws Exception {
-        TokenApprovalStore store = new TokenApprovalStore();
-        store.setTokenStore(tokenStore);
-        return store;
-    }
-
-    // Password encoder
-    @Bean
-    public BCryptPasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
+    @Qualifier("jwkTokenServices")
+    private ResourceServerTokenServices resourceServerTokenServices;
 
     @Override
-    @Order(Ordered.HIGHEST_PRECEDENCE)
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .sessionManagement()
@@ -111,22 +38,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .anonymous().disable()
                 .authorizeRequests()
-                .antMatchers("/**").hasRole("USER")
+                .antMatchers("/auth-server/**", "/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
                 .realmName("API_REALM");
     }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    private OAuth2AuthenticationProcessingFilter oAuth2AuthenticationProcessingFilter() {
+        OAuth2AuthenticationProcessingFilter oAuth2AuthenticationProcessingFilter = new OAuth2AuthenticationProcessingFilter();
+        oAuth2AuthenticationProcessingFilter.setAuthenticationManager(oauthAuthenticationManager());
+        oAuth2AuthenticationProcessingFilter.setStateless(false);
+
+        return oAuth2AuthenticationProcessingFilter;
+    }
+
+    private AuthenticationManager oauthAuthenticationManager() {
+        OAuth2AuthenticationManager oAuth2AuthenticationManager = new OAuth2AuthenticationManager();
+        oAuth2AuthenticationManager.setResourceId("apigateway");
+        oAuth2AuthenticationManager.setTokenServices(resourceServerTokenServices);
+        oAuth2AuthenticationManager.setClientDetailsService(null);
+
+        return oAuth2AuthenticationManager;
     }
 }
