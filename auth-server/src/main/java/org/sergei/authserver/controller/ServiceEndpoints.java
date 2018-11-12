b5444b6f162039895f66f87ddf9de8c282f17489
Sergei <sergei.visotsky@gmail.com>
2018-11-12 19:20:53 +0200

    auth-server development continues

---
+++ auth-server/src/main/java/org/sergei/authserver/controller/ServiceEndpoints.java
@@ -0,0 +1,7 @@
+package org.sergei.authserver.controller;
+
+/**
+ * @author Sergei Visotsky, 2018
+ */
+public class ServiceEndpoints {
+}
--- zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
@@ -2,14 +2,12 @@ package com.example.zuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableZuulProxy
 public class ZuulGatewayApplication {
 
