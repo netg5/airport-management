12d221f1c1f629382faed5d7c5a0abedf9e0752e
Sergei <sergei.visotsky@gmail.com>
2018-11-14 09:19:10 +0200

    Test set to be ignored

--- zuul-gateway/src/test/java/org/sergei/zuulgateway/ZuulGatewayApplicationTests.java
+++ zuul-gateway/src/test/java/org/sergei/zuulgateway/ZuulGatewayApplicationTests.java
@@ -1,16 +1,18 @@
 package org.sergei.zuulgateway;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ZuulGatewayApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
