8623b262157322b9ee0c255ad65af0716fcdb17a
Sergei <sergei.visotsky@gmail.com>
2018-11-19 18:38:06 +0200

    Swagger docs for flight reservation service allowed

---
+++ flight-service/src/main/java/org/sergei/flightservice/config/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package org.sergei.flightservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Sergei Visotsky, 2018
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/docs", "/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**");
+    }
+}
