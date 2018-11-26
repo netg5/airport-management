36688b8029e33451de0ed48250340d7bb30791b4
Sergei <sergei.visotsky@gmail.com>
2018-11-26 19:07:30 +0200

    Initial commit of ticket service

---
+++ ticket-service/src/main/java/org/sergei/ticketservice/config/WebMvcConfig.java
@@ -0,0 +1,29 @@
+package org.sergei.ticketservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+/**
+ * @author Sergei Visotsky, 2018
+ */
+@Configuration
+@Import(SwaggerConfig.class)
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LocaleChangeInterceptor());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
