8d338df06b4b04b90eafc20f6729008aefcf695a
Sergei <sergei.visotsky@gmail.com>
2018-11-26 19:21:11 +0200

    Test set to be ignored

--- ticket-service/src/test/java/org/sergei/ticketservice/TicketServiceApplicationTests.java
+++ ticket-service/src/test/java/org/sergei/ticketservice/TicketServiceApplicationTests.java
@@ -1,16 +1,18 @@
 package org.sergei.ticketservice;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TicketServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
