c503678dcaa20f812f8bbc0cdf25588f61a1204a
Sergei <sergei.visotsky@gmail.com>
2018-11-28 21:21:15 +0200

    Code cleanup

--- ticket-service/src/main/java/org/sergei/ticketservice/repository/TicketRepository.java
+++ ticket-service/src/main/java/org/sergei/ticketservice/repository/TicketRepository.java
@@ -2,8 +2,6 @@ package org.sergei.ticketservice.repository;
 
 import org.sergei.ticketservice.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +12,6 @@ import java.util.List;
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
-//    @Query("SELECT t FROM Ticket t WHERE t.customerId = :customerId")
+    //    @Query("SELECT t FROM Ticket t WHERE t.customerId = :customerId")
     List<Ticket> findAllByCustomerId(Long customerId);
 }
