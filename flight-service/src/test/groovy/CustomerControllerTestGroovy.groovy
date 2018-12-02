b9b51a0be48e13696fcf41510cb417d05e812ff3
Sergei <sergei.visotsky@gmail.com>
2018-12-02 15:53:23 +0200

    Data in lombock removed

--- flight-service/src/test/groovy/CustomerControllerTestGroovy.groovy
+++ flight-service/src/test/groovy/CustomerControllerTestGroovy.groovy
@@ -13,7 +13,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@Ignore
 @WebMvcTest(controllers = CustomerController)
 @ContextConfiguration(classes = WebSecurityConfig.class)
 class CustomerControllerTestGroovy extends Specification {
