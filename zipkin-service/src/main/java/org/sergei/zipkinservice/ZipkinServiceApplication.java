f77d9d15ee840e704243a6f242bc89cb228693ae
Sergei <sergei.visotsky@gmail.com>
2018-12-03 16:51:50 +0200

    Ports updated in Dockerfile, minor property updates

--- zipkin-service/src/main/java/org/sergei/zipkinservice/ZipkinServiceApplication.java
+++ zipkin-service/src/main/java/org/sergei/zipkinservice/ZipkinServiceApplication.java
@@ -7,6 +7,7 @@ import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZipkinServer
 public class ZipkinServiceApplication {
 
     public static void main(String[] args) {
