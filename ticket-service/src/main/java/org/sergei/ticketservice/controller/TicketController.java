a7c55d1dfecc068ad0a9486ad16ebabc43aaa601
Sergei <sergei.visotsky@gmail.com>
2018-12-15 19:04:40 +0200

    link fixed

--- ticket-service/src/main/java/org/sergei/ticketservice/controller/TicketController.java
+++ ticket-service/src/main/java/org/sergei/ticketservice/controller/TicketController.java
@@ -66,7 +66,7 @@ public class TicketController {
         Resources<Ticket> resources = new Resources<>(ticketList);
         String uriString = ServletUriComponentsBuilder.fromCurrentRequest().build().toUriString();
         resources.add(new Link(uriString, "self"));
-        resources.add(new Link("http://127.0.0.1git :8080/flight-api/customers/" + customerId, "customer"));
+        resources.add(new Link("http://127.0.0.1:8080/flight-api/customers/" + customerId, "customer"));
         return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 }
