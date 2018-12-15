5e81ed86a8d838e3fa6084dc8e4851eb220d30dc
Sergei <sergei.visotsky@gmail.com>
2018-12-15 14:48:15 +0200

    pagination inticket service added

--- ticket-service/src/main/java/org/sergei/ticketservice/controller/TicketController.java
+++ ticket-service/src/main/java/org/sergei/ticketservice/controller/TicketController.java
@@ -6,6 +6,8 @@ import io.swagger.annotations.ApiParam;
 import org.sergei.ticketservice.model.Ticket;
 import org.sergei.ticketservice.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
@@ -42,6 +44,25 @@ public class TicketController {
                                                                @ApiParam(value = "Distance with which ticket should be found")
                                                                @RequestParam(value = "distance", required = false) Double distance) {
         List<Ticket> ticketList = ticketRepository.findByCustomerIdPlaceOrDistance(customerId, place, distance);
+        Resources<Ticket> resources = new Resources<>(ticketList);
+        String uriString = ServletUriComponentsBuilder.fromCurrentRequest().build().toUriString();
+        resources.add(new Link(uriString, "self"));
+        return new ResponseEntity<>(resources, HttpStatus.OK);
+    }
+
+    @ApiOperation("Get ticket for customer by ID")
+    @GetMapping(params = {"page", "size"})
+    public ResponseEntity<Resources<Ticket>> getAllForCustomerPaginated(@ApiParam(value = "Customer ID whose ticket should be found", required = true)
+                                                                        @RequestParam("customerId") Long customerId,
+                                                                        @ApiParam(value = "Place with which ticket should be found")
+                                                                        @RequestParam(value = "place", required = false) String place,
+                                                                        @ApiParam(value = "Distance with which ticket should be found")
+                                                                        @RequestParam(value = "distance", required = false) Double distance,
+                                                                        @ApiParam(value = "Number of page", required = true)
+                                                                        @RequestParam("page") int page,
+                                                                        @ApiParam(value = "Number of elements per page", required = true)
+                                                                        @RequestParam("size") int size) {
+        Page<Ticket> ticketList = ticketRepository.findByCustomerIdPlaceOrDistancePageable(customerId, place, distance, PageRequest.of(page, size));
         Resources<Ticket> resources = new Resources<>(ticketList);
         String uriString = ServletUriComponentsBuilder.fromCurrentRequest().build().toUriString();
         resources.add(new Link(uriString, "self"));
--- ticket-service/src/main/java/org/sergei/ticketservice/repository/TicketRepository.java
+++ ticket-service/src/main/java/org/sergei/ticketservice/repository/TicketRepository.java
@@ -1,6 +1,8 @@
 package org.sergei.ticketservice.repository;
 
 import org.sergei.ticketservice.model.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -18,4 +20,10 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
             "AND (?2 IS NULL OR t.place = ?2) " +
             "AND (?3 IS NULL OR t.distance = ?3)")
     List<Ticket> findByCustomerIdPlaceOrDistance(Long customerId, String place, Double distance);
+
+    @Query("SELECT t FROM Ticket t WHERE t.customerId = ?1 " +
+            "AND (?2 IS NULL OR t.place = ?2) " +
+            "AND (?3 IS NULL OR t.distance = ?3)")
+    Page<Ticket> findByCustomerIdPlaceOrDistancePageable(Long customerId, String place,
+                                                         Double distance, Pageable pageable);
 }
