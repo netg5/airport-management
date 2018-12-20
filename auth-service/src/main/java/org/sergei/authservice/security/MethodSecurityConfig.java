02dc5cb2d97469d54d77af8ab18e9463c12d387e
Sergei <sergei.visotsky@gmail.com>
2018-12-20 17:10:57 +0200

    Development of method availability for a particular role in SSO started

---
+++ auth-service/src/main/java/org/sergei/authservice/security/MethodSecurityConfig.java
@@ -0,0 +1,23 @@
+package org.sergei.authservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+
+/**
+ * This class needs for role delegation of the API resources.
+ * For example allow admin to delete any resource only
+ *
+ * @author Sergei Visotsky
+ */
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+
+    @Override
+    protected MethodSecurityExpressionHandler createExpressionHandler() {
+        return new OAuth2MethodSecurityExpressionHandler();
+    }
+}
