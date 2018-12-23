fd125825c51306c53675c6cf4ffafcfeee7f2657
Sergei <sergei.visotsky@gmail.com>
2018-12-23 09:13:34 +0200

    Comments added

--- flight-service/src/main/java/org/sergei/flightservice/service/IReservationService.java
+++ flight-service/src/main/java/org/sergei/flightservice/service/IReservationService.java
@@ -9,15 +9,59 @@ import java.util.Map;
  * @author Sergei Visotsky
  */
 public interface IReservationService<E, T> extends IService {
-    E findOneForCustomer(Long aLong, Long aLong2);
 
+    /**
+     * Find one reservation for customer
+     *
+     * @param customerId    Customer ID
+     * @param reservationId reservation ID
+     * @return reservation entity
+     */
+    E findOneForCustomer(Long customerId, Long reservationId);
+
+    /**
+     * Find all reservations for customer
+     *
+     * @param customerId customer ID to find reservations
+     * @return list of reservations
+     */
     List<E> findAllForCustomer(Long customerId);
 
+    /**
+     * Find all reservations for customer paginated
+     *
+     * @param customerId Customer ID whoose reservations should be found
+     * @param page       number of page to show
+     * @param size       number of elements per page
+     * @return collection of entities
+     */
     Page<E> findAllForCustomerPaginated(Long customerId, int page, int size);
 
+    /**
+     * Save reservation for customer
+     *
+     * @param aLong     Customer ID who made reservation
+     * @param entityDTO reservation body
+     * @return saved reservation
+     */
     T saveReservation(Long aLong, T entityDTO);
 
-    T updateReservation(Long aLong, Long aLong2, T entityDTO);
+    /**
+     * Update reservation
+     *
+     * @param customerId    ID of the customer who made reservation
+     * @param reservationId reservation ID to be updated
+     * @param entityDTO     updated reservation body
+     * @return updated reservation
+     */
+    T updateReservation(Long customerId, Long reservationId, T entityDTO);
 
+    /**
+     * Patch one or multiple fields of reservation
+     *
+     * @param aLong  reservation ID to be patched
+     * @param params Field(-s) to be patched
+     * @return patched reservation
+     */
     T patchReservation(Long aLong, Map<String, Object> params);
 }
--- flight-service/src/main/java/org/sergei/flightservice/service/IRouteService.java
+++ flight-service/src/main/java/org/sergei/flightservice/service/IRouteService.java
@@ -8,9 +8,28 @@ import java.util.List;
  * @author Sergei Visotsky
  */
 public interface IRouteService<E, T> extends IService<E> {
+
+    /**
+     * Find one route
+     *
+     * @param aLong ID of the route to be found
+     * @return route body to be returned
+     */
     T findOneRoute(Long aLong);
 
+    /**
+     * Find list of routes
+     *
+     * @return list of all routes found
+     */
     List<T> findAllRoutes();
 
+    /**
+     * Find all routed paginated
+     *
+     * @param page number of page to return
+     * @param size number of elements per page
+     * @return page of entities
+     */
     Page<T> findAllRoutesPaginated(int page, int size);
 }
