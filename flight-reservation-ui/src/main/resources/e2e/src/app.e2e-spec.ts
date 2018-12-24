6401906ac44a6771c52b3c27ce14f782114ade98
Sergei <sergei.visotsky@gmail.com>
2018-12-24 09:31:07 +0200

    gitignore upadte

--- flight-reservation-ui/src/main/resources/e2e/src/app.e2e-spec.ts
+++ flight-reservation-ui/src/main/resources/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to flight-reservation-ui!');
+    expect(page.getTitleText()).toEqual('Welcome to resources!');
   });
 });
