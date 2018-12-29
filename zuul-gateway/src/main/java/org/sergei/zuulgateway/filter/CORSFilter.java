4fe7652da703cc260a653d253ff4003325c88fb7
Sergei <sergei.visotsky@gmail.com>
2018-12-29 08:54:11 +0200

    CorsFilter fixed

--- zuul-gateway/src/main/java/org/sergei/zuulgateway/filter/CORSFilter.java
+++ zuul-gateway/src/main/java/org/sergei/zuulgateway/filter/CORSFilter.java
@@ -29,8 +29,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Heade" +
-                "rs", "*");
+        response.setHeader("Access-Control-Allow-Headers", "*");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
