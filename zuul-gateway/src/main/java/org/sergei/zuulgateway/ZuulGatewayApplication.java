d2156f739b33524210e13759c14a3ad4bb3823c1
Sergei <sergei.visotsky@gmail.com>
2018-12-30 21:12:39 +0200

    Polishing

--- zuul-gateway/src/main/java/org/sergei/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/org/sergei/zuulgateway/ZuulGatewayApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
