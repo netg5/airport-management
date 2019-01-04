84eda78728f5aa31d30b29e366dfa7e12c62517b
Sergei <sergei.visotsky@gmail.com>
2019-01-04 19:10:14 +0200

    Logging aspect for authentication service added

---
+++ auth-service/src/main/java/org/sergei/authservice/config/LoggingAspectConfiguration.java
@@ -0,0 +1,19 @@
+package org.sergei.authservice.config;
+
+import org.sergei.authservice.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author Sergei Visotsky
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
+}
