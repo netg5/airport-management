8de4904cc573d6b1b5447da4f8bd26fbf8696618
Sergei <sergei.visotsky@gmail.com>
2019-01-05 08:35:26 +0200

    Dockerfiles updated

--- service-resource/src/test/java/org/sergei/serviceresource/ServiceEndpointTest.java
+++ service-resource/src/test/java/org/sergei/serviceresource/ServiceEndpointTest.java
@@ -1,6 +1,7 @@
 package org.sergei.serviceresource;
 
 import org.json.JSONObject;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
