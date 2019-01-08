83061d054a2fd1028abbd9cf8ddefc3fa3de0312
Sergei <sergei.visotsky@gmail.com>
2019-01-08 11:59:29 +0200

    constructor of the class with constants made private

--- ticket-service/src/main/java/org/sergei/ticketservice/service/Constants.java
+++ ticket-service/src/main/java/org/sergei/ticketservice/service/Constants.java
@@ -6,6 +6,13 @@ package org.sergei.ticketservice.service;
  * @author Sergei Visotsky
  */
 final class Constants {
+
+    /**
+     * Hide from the public access
+     */
+    private Constants() {
+    }
+
     static final String TICKETS_NOT_FOUND = "Customer has no tickets";
     static final String CUSTOMER_NOT_FOUND = "Customer with this ID not found";
 }
