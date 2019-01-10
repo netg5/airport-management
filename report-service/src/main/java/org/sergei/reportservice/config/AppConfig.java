ff5abb8c3113973e4e0a9ee497f2278660638a5d
Sergei <sergei.visotsky@gmail.com>
2019-01-10 20:47:35 +0200

    Initial commit of report service

---
+++ report-service/src/main/java/org/sergei/reportservice/config/AppConfig.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.sergei.reportservice.config;
+
+import org.sergei.reportservice.aop.LoggingAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author Sergei Visotsky
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class AppConfig {
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.setMaxAge(3600L);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
---
+++ report-service/src/main/java/org/sergei/reportservice/config/SwaggerConfig.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.sergei.reportservice.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.security.oauth2.provider.token.AccessTokenConverter.CLIENT_ID;
+
+/**
+ * @author Sergei Visotsky
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SwaggerConfig.class);
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Value("${security.oauth2.access-token-uri}")
+    private String authServer;
+
+    @Value("${spring.getaway.port}")
+    private int port;
+
+    private static final String CLIENT_SECRET = "client_secret";
+
+    @Bean
+    public Docket api() {
+        LOGGER.debug("Server port id: {}", port);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping(contextPath)
+                .host("localhost:" + port)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.sergei.reportservice.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Collections.singletonList(securitySchema()))
+                .securityContexts(Collections.singletonList(securityContext()))
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Report service REST endpoints")
+                .description("Methods needed to retrieve all data to create reports for a specific model")
+                .version("1.0")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .contact(new Contact("", "", "sergei.visotsky@gmail.com"))
+                .build();
+    }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder()
+                .clientId(CLIENT_ID)
+                .clientSecret(CLIENT_SECRET)
+                .scopeSeparator("")
+                .useBasicAuthenticationWithAccessCodeGrant(true)
+                .build();
+    }
+
+    private SecurityScheme securitySchema() {
+        List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
+        authorizationScopeList.add(new AuthorizationScope("read", "read all"));
+        authorizationScopeList.add(new AuthorizationScope("trust", "trust all"));
+        authorizationScopeList.add(new AuthorizationScope("write", "write all"));
+
+        List<GrantType> grantTypes = new ArrayList<>();
+        LOGGER.debug("OAuth2 token URI is: {}", authServer);
+        GrantType grantType = new ResourceOwnerPasswordCredentialsGrant(authServer);
+        grantTypes.add(grantType);
+
+        return new OAuth("oauth2schema", authorizationScopeList, grantTypes);
+    }
+
+    private List<SecurityReference> defaultAuth() {
+
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[3];
+        authorizationScopes[0] = new AuthorizationScope("read", "read all");
+        authorizationScopes[1] = new AuthorizationScope("trust", "trust all");
+        authorizationScopes[2] = new AuthorizationScope("write", "write all");
+
+        return Collections.singletonList(new SecurityReference("oauth2schema", authorizationScopes));
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext
+                .builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.ant("/**"))
+                .build();
+    }
+}
