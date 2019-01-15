17816f4802053019153f9a551b4ee77f5fb68513
Sergei <sergei.visotsky@gmail.com>
2019-01-15 10:22:55 +0200

    Angular front-end replaced with Spring + thymeleaf

---
+++ front-end-service/src/main/java/org/sergei/frontendservice/FrontEndServiceApplication.java
@@ -0,0 +1,16 @@
+package org.sergei.frontendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FrontEndServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontEndServiceApplication.class, args);
+    }
+
+}
+
