7bf6c2998eb2b32211de3ee214cd35ca905ff447
Sergei <sergei.visotsky@gmail.com>
2019-01-18 20:51:10 +0200

    Ticket classes and html page added

---
+++ front-end-service/src/main/java/org/sergei/frontendservice/controller/TicketController.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018-2019 the original author.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.sergei.frontendservice.controller;
+
+import org.sergei.frontendservice.model.Ticket;
+import org.sergei.frontendservice.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Sergei Visotsky
+ */
+@Controller
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    @Autowired
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @GetMapping("/customers/{customerId}/tickets")
+    public String showCustomerTickets(@PathVariable Long customerId, Model model) throws IOException {
+        ResponseEntity<List<Ticket>> tickets = ticketService.findAllTicketsByCustomerId(customerId);
+        List<Ticket> ticketsResponseBody = tickets.getBody();
+        model.addAttribute("firstName", Objects.requireNonNull(ticketsResponseBody).get(0).getFirstName());
+        model.addAttribute("lastName", ticketsResponseBody.get(0).getLastName());
+        model.addAttribute("routeId", ticketsResponseBody.get(0).getRouteId());
+        model.addAttribute("place", ticketsResponseBody.get(0).getPlace());
+        model.addAttribute("distance", ticketsResponseBody.get(0).getDistance());
+        model.addAttribute("price", ticketsResponseBody.get(0).getPrice());
+        model.addAttribute("aircraftName", ticketsResponseBody.get(0).getAircraftName());
+        return "tickets";
+    }
+}
---
+++ front-end-service/src/main/java/org/sergei/frontendservice/service/TicketService.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018-2019 the original author.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.sergei.frontendservice.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.sergei.frontendservice.model.AuthTokenInfo;
+import org.sergei.frontendservice.model.Reservation;
+import org.sergei.frontendservice.model.Ticket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Sergei Visotsky
+ */
+@Service
+public class TicketService {
+
+    private static final String TICKET_API_URI = "https://localhost:9090/ticket-api";
+    private static final String TICKETS_PATH = "/tickets";
+    private static final String CUSTOMER_ID_PARAM = "?customerId=";
+    private static final String ACCESS_TOKEN = "&access_token=";
+
+    private final RestTemplate restTemplate;
+    private final TokenRetrievalService tokenRetrievalService;
+
+    @Autowired
+    public TicketService(RestTemplate restTemplate, TokenRetrievalService tokenRetrievalService) {
+        this.restTemplate = restTemplate;
+        this.tokenRetrievalService = tokenRetrievalService;
+    }
+
+    public ResponseEntity<List<Ticket>> findAllTicketsByCustomerId(Long customerId) throws IOException {
+        AuthTokenInfo tokenInfo = tokenRetrievalService.sendTokenRequest();
+        HttpEntity<String> request = new HttpEntity<>(tokenRetrievalService.getHeaders());
+        ResponseEntity<String> responseEntity = this.restTemplate.exchange(TICKET_API_URI +
+                        TICKETS_PATH + CUSTOMER_ID_PARAM + customerId + ACCESS_TOKEN + tokenInfo.getAccessToken(),
+                HttpMethod.GET, request, String.class);
+
+        String data = responseEntity.getBody();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        JsonNode jsNode = objectMapper.readTree(data);
+        String nodeAt = jsNode.at("/_embedded/ticketList").toString();
+
+        return objectMapper.readValue(nodeAt, new TypeReference<ResponseEntity<List<Reservation>>>() {
+        });
+    }
+}
