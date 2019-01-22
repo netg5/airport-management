d8fee1fccb4e14c1b832a205c183334cfc17b9c7
Sergei <sergei.visotsky@gmail.com>
2019-01-22 09:49:59 +0200

    code cleanup

--- front-end-service/src/main/java/org/sergei/frontendservice/controller/RouteController.java
+++ front-end-service/src/main/java/org/sergei/frontendservice/controller/RouteController.java
@@ -25,8 +25,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Objects;
-
 /**
  * @author Sergei Visotsky
  */
