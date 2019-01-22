90a2cff00df98064a15b702a639533a7239c6eb9
Sergei <sergei.visotsky@gmail.com>
2019-01-22 09:01:38 +0200

    Report API accessible for ROLE_ADMIN only by now

--- report-service/src/main/java/org/sergei/reportservice/controller/CustomerReportController.java
+++ report-service/src/main/java/org/sergei/reportservice/controller/CustomerReportController.java
@@ -27,7 +27,6 @@ import org.sergei.reportservice.service.CustomerReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
