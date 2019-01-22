59a1d8340f6ffa1cb16e11fd51432ef14e97951c
Sergei <sergei.visotsky@gmail.com>
2019-01-22 09:01:19 +0200

    Report API accessible for ROLE_ADMIN only by now

--- report-service/src/main/java/org/sergei/reportservice/controller/AircraftReportController.java
+++ report-service/src/main/java/org/sergei/reportservice/controller/AircraftReportController.java
@@ -27,7 +27,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 /**
