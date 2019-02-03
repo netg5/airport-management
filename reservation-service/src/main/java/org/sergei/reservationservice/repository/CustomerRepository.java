dc8262d8e007fd3830017ac49ba171fbde83208f
Sergei <sergei.visotsky@gmail.com>
2019-02-03 21:55:20 +0200

    SQL fixed

--- reservation-service/src/main/java/org/sergei/reservationservice/repository/CustomerRepository.java
+++ reservation-service/src/main/java/org/sergei/reservationservice/repository/CustomerRepository.java
@@ -40,7 +40,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
      *
      * @return list of IDs
      */
-    @Query("SELECT NEW org.sergei.reservationservice.dto.CustomerIdsDTO(c.customerId) FROM Customer c")
+    @Query("SELECT NEW org.sergei.reservationservice.rest.dto.CustomerIdsDTO(c.customerId) FROM Customer c")
     List<CustomerIdsDTO> findIdsOfAllCustomers();
 
     /**
@@ -51,7 +51,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
      * @param pageable number of the page and number of the elements per page
      * @return Collection of customer IDs
      */
-    @Query(value = "SELECT NEW org.sergei.reservationservice.dto.CustomerIdsDTO(c.customerId) FROM Customer c",
+    @Query(value = "SELECT NEW org.sergei.reservationservice.rest.dto.CustomerIdsDTO(c.customerId) FROM Customer c",
             countQuery = "SELECT count(c) FROM Customer c")
     Page<CustomerIdsDTO> findIdsOfAllCustomersPaginated(Pageable pageable);
 }
