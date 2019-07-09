1b63174b77955cc20ef30335337f82f99cb2db57
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-07-09 21:36:20 +0300

    @Profile added

--- auth/src/main/java/org/sergei/authservice/rest/EncryptionEndpoint.java
+++ auth/src/main/java/org/sergei/authservice/rest/EncryptionEndpoint.java
@@ -20,6 +20,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,6 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
         value = "/encryption",
         produces = "application/json"
 )
+@Profile({"dev"})
 @RestController
 public class EncryptionEndpoint {
 
