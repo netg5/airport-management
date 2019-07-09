ce5f81213ac1d09abaac7f1aacde7074326fc8f7
Sergei Visotsky <sergejs.visockis@emergn.com>
2019-07-09 18:11:12 +0300

    Lombok for logging implemented

--- auth/src/main/java/org/sergei/authservice/config/security/OAuthSchemaConfig.java
+++ auth/src/main/java/org/sergei/authservice/config/security/OAuthSchemaConfig.java
@@ -16,8 +16,7 @@
 
 package org.sergei.authservice.config.security;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,9 +33,8 @@ import javax.sql.DataSource;
  * @author Sergei Visotsky
  */
 @Configuration
+@Slf4j
 public class OAuthSchemaConfig {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorizationServerConfig.class);
 
     @Value("classpath:sql/oauth_schema.sql")
     private Resource schemaScript;
@@ -46,14 +44,14 @@ public class OAuthSchemaConfig {
         DataSourceInitializer initializer = new DataSourceInitializer();
         initializer.setDataSource(dataSource);
         initializer.setDatabasePopulator(databasePopulator());
-        LOGGER.debug("Database set");
+        log.debug("Database set");
         return initializer;
     }
 
     private DatabasePopulator databasePopulator() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScript(schemaScript);
-        LOGGER.debug("Database schema created");
+        log.debug("Database schema created");
         return populator;
     }
 }
