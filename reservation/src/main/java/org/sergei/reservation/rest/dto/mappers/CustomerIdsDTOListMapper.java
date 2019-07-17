742d07192fdf392db404e16a97ca4afac4421f96
Sergei Visotsky <sergejs.visockis@emergn.com>
2019-07-17 19:14:50 +0300

    Redundant mapper removed

--- reservation/src/main/java/org/sergei/reservation/rest/dto/mappers/CustomerIdsDTOListMapper.java
+++
@@ -1,9 +0,0 @@
-package org.sergei.reservation.rest.dto.mappers;
-
-import java.util.List;
-
-/**
- * @author Sergei Visotsky
- */
-public class CustomerIdsDTOListMapper implements IMapper<List> {
-}
