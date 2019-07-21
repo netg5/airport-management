4a75a4387cfc4c1d8eb094188b238caf1b6df0c5
Sergei Visotsky <sergejs.visockis@emergn.com>
2019-07-21 16:11:54 +0300

    Serializer config

--- reservation/src/main/java/org/sergei/reservation/config/ApplicationConfig.java
+++ reservation/src/main/java/org/sergei/reservation/config/ApplicationConfig.java
@@ -16,17 +16,33 @@
 
 package org.sergei.reservation.config;
 
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * @author Sergei Visotsky
  */
 @Configuration
+@EnableWebMvc
 @Import(SwaggerConfig.class)
 @ComponentScan({
         "org.sergei.reservation"
 })
-public class ApplicationConfig {
+public class ApplicationConfig implements WebMvcConfigurer {
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter converter = (MappingJackson2HttpMessageConverter) converters.stream()
+                .filter(c -> c instanceof MappingJackson2HttpMessageConverter).findFirst().get();
+        SimpleModule module = new SimpleModule();
+        converter.getObjectMapper().registerModule(module);
+    }
 }
