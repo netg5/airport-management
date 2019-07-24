4cd2c171834a0739d9e523bff12b0688bf241335
Sergei Visotsky <sergejs.visockis@emergn.com>
2019-07-24 13:58:40 +0300

    Modification

---
+++ new-ui/src/CustomerApi.tsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import './static/css/style.css';
+
+const jsonHeader = {
+    'Content-Type': "application/json"
+};
+
+class CusotmerApi extends Component {
+    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+        return ();
+    }
+
+    getCustomerById(cusomerId) {
+        const url = 'http://localhost:8080/reservations/customers/' + cusomerId;
+        return axios
+            .get(url)
+            .then(response => response.data);
+    }
+}
