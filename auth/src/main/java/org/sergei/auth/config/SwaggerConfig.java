a4b3ed3e4930aa20909ef0afeeb6be7832ee0a94
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-07-25 22:21:33 +0300

    Security removed from swaggger due to it is available from the dev profile only

--- auth/src/main/java/org/sergei/auth/config/SwaggerConfig.java
+++ auth/src/main/java/org/sergei/auth/config/SwaggerConfig.java
@@ -16,40 +16,22 @@
 
 package org.sergei.auth.config;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.SecurityConfiguration;
-import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static org.springframework.security.oauth2.provider.token.AccessTokenConverter.CLIENT_ID;
 
 /**
  * @author Sergei Visotsky
  */
-@Slf4j
 @Configuration
 @EnableSwagger2
 @Profile({"dev"})
 public class SwaggerConfig {
-
-    @Value("${security.oauth2.access-token-uri}")
-    private String authServer;
-
-    private static final String CLIENT_SECRET = "client_secret";
 
     @Bean
     public Docket api() {
@@ -57,50 +39,6 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.sergei.auth.rest"))
                 .paths(PathSelectors.any())
-                .build()
-                .securitySchemes(Collections.singletonList(securitySchema()))
-                .securityContexts(Collections.singletonList(securityContext()));
-    }
-
-    @Bean
-    public SecurityConfiguration security() {
-        return SecurityConfigurationBuilder.builder()
-                .clientId(CLIENT_ID)
-                .clientSecret(CLIENT_SECRET)
-                .scopeSeparator("")
-                .useBasicAuthenticationWithAccessCodeGrant(true)
-                .build();
-    }
-
-    private SecurityScheme securitySchema() {
-        List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
-        authorizationScopeList.add(new AuthorizationScope("read", "read all"));
-        authorizationScopeList.add(new AuthorizationScope("trust", "trust all"));
-        authorizationScopeList.add(new AuthorizationScope("write", "write all"));
-
-        List<GrantType> grantTypes = new ArrayList<>();
-        log.debug("OAuth2 token URI is: {}", authServer);
-        GrantType grantType = new ResourceOwnerPasswordCredentialsGrant(authServer);
-        grantTypes.add(grantType);
-
-        return new OAuth("oauth2schema", authorizationScopeList, grantTypes);
-    }
-
-    private List<SecurityReference> defaultAuth() {
-
-        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[3];
-        authorizationScopes[0] = new AuthorizationScope("read", "read all");
-        authorizationScopes[1] = new AuthorizationScope("trust", "trust all");
-        authorizationScopes[2] = new AuthorizationScope("write", "write all");
-
-        return Collections.singletonList(new SecurityReference("oauth2schema", authorizationScopes));
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext
-                .builder()
-                .securityReferences(defaultAuth())
-                .forPaths(PathSelectors.ant("/**"))
                 .build();
     }
 }
