eae5dc791f45000ca370e08269970e1908e29484
Sergei Visotsky <sergejs.visockis@emergn.com>
2019-07-26 10:03:51 +0300

    Swagger availability only with access token and in prod only

--- tickets/src/main/java/org/sergei/tickets/config/security/WebSecurityConfig.java
+++ tickets/src/main/java/org/sergei/tickets/config/security/WebSecurityConfig.java
@@ -27,6 +27,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/docs", "/v2/api-docs", "/configuration/**", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**");
+        web.ignoring().antMatchers("/docs", "/v2/api-docs", "/configuration/**", "/swagger-resources/**", "/webjars/**");
     }
 }
