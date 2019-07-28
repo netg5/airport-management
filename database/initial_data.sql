b28f961793d0ed4172e5baa553703deb644832ca
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-07-28 18:00:11 +0300

    Initial schema and schema update

--- database/initial_data.sql
+++ database/initial_data.sql
@@ -1,20 +1,23 @@
-INSERT INTO aircraft
-VALUES (1, '123D', 1, '213D34', '777-300ER', 'Boeing', 3450, 1000);
+INSERT INTO pilot VALUES (1, 'DAS124', '12-34-56', 'John', 'Smith', 'male', 50.0, '1970-08-29', '3rd Kennedy str, New-York', 'USA','someowner@somewhere.com', '1234567');
+INSERT INTO pilot VALUES (2, 'BD9051', '90-A1-9K', 'Janis', 'Priede', 'male', 60.0, '1985-08-20', '5th Barona str, Riga', 'Latvia','someowner@somewhere.com', '34534625');
+INSERT INTO pilot VALUES (3, 'LK4562', '1Q-3B-23', 'Fiona', 'Pridaine', 'female', 85.0, '1995-05-29', '1st Amerino str, Berlin', 'Germany','someowner@somewhere.com', '437372342');
 
-INSERT INTO aircraft
-VALUES (2, 'Airbus', 43000, 5400, 'A220');
+INSERT INTO owner VALUES (1, 'Alexey', 'Vishnyakov', 'male', '3rd Kennedy str, New-York', 'USA', 'someowner@somewhere.com', '1234567');
+INSERT INTO owner VALUES (2, 'John', 'Denny', 'male', '5rd Avenue str, New-York', 'USA', 'someowner@somewhere.com', '1234567');
+INSERT INTO owner VALUES (3, 'Jane', 'Smith', 'female', '12th Elizabeth str, London', 'England', 'someowner@somewhere.com', '1234567');
 
-INSERT INTO aircraft
-VALUES (3, 'Airbus', 42007, 3200, '767');
-INSERT INTO customer
-VALUES (1, 25, 'John', 'Smith');
-INSERT INTO customer
-VALUES (2, 21, 'Sergei', 'Visotsky');
-INSERT INTO customer
-VALUES (3, 38, 'Krist', 'Kronfold');
-INSERT INTO route
-VALUES (1, '2019-08-23', '2019-08-22', 3600, 'New-York', 3400, 1);
-INSERT INTO route
-VALUES (2, '2019-07-02', '2019-07-01', 1000, 'Riga', 123, 2);
-INSERT INTO route
-VALUES (3, '2019-08-23', '2019-08-22', 3600, 'Singapoore', 3400, 3);
+INSERT INTO aircraft VALUES (1, '123D', 1, '213D34', '777-300ER', 'Boeing', 3450, 1000, 3);
+INSERT INTO aircraft VALUES (2, '12dc3', 2, '23d', 'A220', 'Airbus', 43000, 5400, 6);
+INSERT INTO aircraft VALUES (3, '82497Df', 3, '38fc12', 'B30', 'Airbus', 42007, 3200, 7);
+
+INSERT INTO route VALUES (2, '2019-07-02', '2019-07-01', 1000, 'Riga', 123, 2);
+INSERT INTO route VALUES (3, '2019-08-23', '2019-08-22', 3600, 'Singapoore', 3400, 3);
+INSERT INTO route VALUES (1, '2019-08-23', '2019-08-22', 3600, 'New-York', 3400, 1);
+
+INSERT INTO passenger VALUES (1, 'Arena', 'Kronfold', 45, 'female', '8923847');
+INSERT INTO passenger VALUES (2, 'Sergei', 'Visotsky', 21, 'male', '1234567');
+INSERT INTO passenger VALUES (3, 'Krist', 'Kronfold', 29, 'male', '9204');
+
+INSERT INTO manager VALUES (1, '12-34-56', 'John', 'Smith', 'male', '3rd Kennedy str, New-York', 'USA','someowner@somewhere.com', '1234567');
+INSERT INTO manager VALUES (2, '90-A1-9K', 'Janis', 'Priede', 'male', '5th Barona str, Riga', 'Latvia','someowner@somewhere.com', '34534625');
+INSERT INTO manager VALUES (3, '1Q-3B-23', 'Fiona', 'Pridaine', 'female', '1st Amerino str, Berlin', 'Germany','someowner@somewhere.com', '437372342');
--- database/schema.sql
+++ database/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE pilot
     last_name      VARCHAR(100)     NOT NULL,
     gender         VARCHAR(6)       NOT NULL,
     weight         DOUBLE PRECISION NOT NULL,
-    date_of_birth  VARCHAR(10)      NOT NULL,
+    date_of_birth  DATE             NOT NULL,
     address        VARCHAR(100)     NOT NULL,
     country        VARCHAR(45)      NOT NULL,
     email          VARCHAR(45)      NOT NULL,
@@ -15,17 +15,30 @@ CREATE TABLE pilot
     CONSTRAINT pilot_pk PRIMARY KEY (id)
 );
 
+CREATE TABLE owner
+(
+    id         BIGINT       NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name  VARCHAR(100) NOT NULL,
+    gender     VARCHAR(6)   NOT NULL,
+    address    VARCHAR(100) NOT NULL,
+    country    VARCHAR(45)  NOT NULL,
+    email      VARCHAR(45)  NOT NULL,
+    phone      NUMERIC(25)  NOT NULL,
+    CONSTRAINT owner_pk PRIMARY KEY (id)
+);
+
 CREATE TABLE aircraft
 (
-    id                  BIGINT                                   NOT NULL,
-    manufacturer_code   VARCHAR(45)                              NOT NULL,
-    owner_id            BIGINT                                   NOT NULL,
-    registration_number VARCHAR(45)                              NOT NULL,
-    model_number        VARCHAR(10)                              NOT NULL,
-    aircraft_name       VARCHAR(45)                              NOT NULL,
-    capacity            INTEGER CHECK (capacity <= 5)            NOT NULL,
-    weight              DOUBLE PRECISION                         NOT NULL,
-    exploitation_period INTEGER CHECK (exploitation_period <= 5) NOT NULL,
+    id                  BIGINT           NOT NULL,
+    manufacturer_code   VARCHAR(45)      NOT NULL,
+    owner_id            BIGINT           NOT NULL,
+    registration_number VARCHAR(45)      NOT NULL,
+    model_number        VARCHAR(10)      NOT NULL,
+    aircraft_name       VARCHAR(45)      NOT NULL,
+    capacity            INTEGER          NOT NULL,
+    weight              DOUBLE PRECISION NOT NULL,
+    exploitation_period INTEGER          NOT NULL,
     CONSTRAINT aircraft_pk PRIMARY KEY (id),
     CONSTRAINT owner_fk FOREIGN KEY (owner_id) REFERENCES owner (id)
 );
@@ -46,7 +59,7 @@ CREATE TABLE manufacturer
 (
     manufacturer_code VARCHAR(60) NOT NULL,
     manufacturer_name VARCHAR(45) NOT NULL,
-    LOCATION          VARCHAR(45) NOT NULL,
+    location          VARCHAR(45) NOT NULL,
     CONSTRAINT manufacturer_pk PRIMARY KEY (manufacturer_code)
 );
 
@@ -73,27 +86,28 @@ CREATE TABLE hangar
     CONSTRAINT airport_id_fk FOREIGN KEY (airport_id) REFERENCES airport (id)
 );
 
-CREATE TABLE customer
+CREATE TABLE passenger
 (
-    id         BIGINT                   NOT NULL,
-    first_name VARCHAR(45)              NOT NULL,
-    last_name  VARCHAR(45)              NOT NULL,
-    age        INTEGER CHECK (age <= 3) NOT NULL,
-    phone      NUMERIC(25)              NOT NULL,
-    CONSTRAINT customer_pk PRIMARY KEY (id)
+    id         BIGINT      NOT NULL,
+    first_name VARCHAR(45) NOT NULL,
+    last_name  VARCHAR(45) NOT NULL,
+    age        INTEGER     NOT NULL,
+    gender     VARCHAR(10) NOT NULL,
+    phone      NUMERIC(25) NOT NULL,
+    CONSTRAINT passenger_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation
 (
     id             BIGINT    NOT NULL,
-    customer_id    BIGINT    NOT NULL,
+    passenger_id   BIGINT    NOT NULL,
     aircraft_id    BIGINT    NOT NULL,
     date_of_flying DATE      NOT NULL,
     departure_time TIMESTAMP NOT NULL,
     arrival_time   TIMESTAMP NOT NULL,
     hours_flying   INTEGER   NOT NULL,
     CONSTRAINT reservation_pk PRIMARY KEY (id),
-    CONSTRAINT customer_id_fk FOREIGN KEY (customer_id) REFERENCES customer (id),
+    CONSTRAINT customer_id_fk FOREIGN KEY (passenger_id) REFERENCES passenger (id),
     CONSTRAINT aircraft_id_fk FOREIGN KEY (aircraft_id) REFERENCES aircraft (id)
 );
 
@@ -109,19 +123,6 @@ CREATE TABLE manager
     email      VARCHAR(45)  NOT NULL,
     phone      NUMERIC(25)  NOT NULL,
     CONSTRAINT manager_pk PRIMARY KEY (id)
-);
-
-CREATE TABLE owner
-(
-    id         BIGINT       NOT NULL,
-    first_name VARCHAR(100) NOT NULL,
-    last_name  VARCHAR(100) NOT NULL,
-    gender     VARCHAR(6)   NOT NULL,
-    address    VARCHAR(100) NOT NULL,
-    country    VARCHAR(45)  NOT NULL,
-    email      VARCHAR(45)  NOT NULL,
-    phone      NUMERIC(25)  NOT NULL,
-    CONSTRAINT owner_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE actual_flight
@@ -148,10 +149,10 @@ CREATE TABLE actual_flight
 
 CREATE TABLE calendar_entry
 (
-    id            BIGINT                          NOT NULL,
-    day_number    INTEGER CHECK (day_number <= 2) NOT NULL,
-    day_date      INTEGER CHECK (day_date <= 2)   NOT NULL,
-    time_in_hours TIMESTAMP                       NOT NULL,
+    id            BIGINT    NOT NULL,
+    day_number    INTEGER   NOT NULL,
+    day_date      INTEGER   NOT NULL,
+    time_in_hours TIMESTAMP NOT NULL,
     CONSTRAINT calendar_entry_pk PRIMARY KEY (id)
 );
 
@@ -172,7 +173,7 @@ CREATE TABLE airport_management_facts
     arrival_time      TIMESTAMP   NOT NULL,
     hours_flying      INTEGER     NOT NULL,
     average           INTEGER     NOT NULL,
-    COUNT             INTEGER     NOT NULL,
+    count             INTEGER     NOT NULL,
     kpi               VARCHAR(45) NOT NULL,
     CONSTRAINT calendar_pk PRIMARY KEY (id),
     CONSTRAINT actual_flight_fk FOREIGN KEY (actual_flight_id) REFERENCES actual_flight (id),
@@ -184,5 +185,4 @@ CREATE TABLE airport_management_facts
     CONSTRAINT manufacturer_fd FOREIGN KEY (manufacturer_code) REFERENCES manufacturer (manufacturer_code),
     CONSTRAINT owner_fk FOREIGN KEY (owner_id) REFERENCES OWNER (id),
     CONSTRAINT pilot_fk FOREIGN KEY (pilot_id) REFERENCES pilot (id)
-
 );
