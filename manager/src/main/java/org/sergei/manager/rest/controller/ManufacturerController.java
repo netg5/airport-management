e8e81b9c878f8b753fb04435d90779dfe1da2acc
Sergei Visotsky <sergejs.visockis@emergn.com>
2019-08-01 18:37:08 +0300

    Manufacturer controller implemented

--- manager/src/main/java/org/sergei/manager/rest/controller/ManufacturerController.java
+++ manager/src/main/java/org/sergei/manager/rest/controller/ManufacturerController.java
@@ -1,8 +1,13 @@
 package org.sergei.manager.rest.controller;
 
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.sergei.manager.rest.dto.ManufacturerDTO;
+import org.sergei.manager.rest.dto.request.ManufacturerRequestDTO;
+import org.sergei.manager.rest.dto.response.ResponseDTO;
+import org.sergei.manager.service.ManufacturerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Sergei Visotsky
@@ -11,5 +16,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/manufacturers")
 @Api(tags = {"manufacturerCrudOperations"})
 public class ManufacturerController {
-    // TODO: Implement
+
+    private final ManufacturerService manufacturerService;
+
+    @Autowired
+    public ManufacturerController(ManufacturerService manufacturerService) {
+        this.manufacturerService = manufacturerService;
+    }
+
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<ResponseDTO<ManufacturerDTO>> getAllManufacturers() {
+        return manufacturerService.findAll();
+    }
+
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public ResponseEntity<ResponseDTO<ManufacturerDTO>> getManufacturerByCode(@RequestBody ManufacturerRequestDTO request) {
+        return manufacturerService.findByCode(request);
+    }
+
+    @PostMapping(value = "/save", produces = "application/json", consumes = "application/json")
+    public ResponseEntity<ResponseDTO<ManufacturerDTO>> saveManufacturer(@RequestBody ManufacturerDTO request) {
+        return manufacturerService.saveManufacturer(request);
+    }
+
+    @PostMapping(value = "/update", produces = "application/json", consumes = "application/json")
+    public ResponseEntity<ResponseDTO<ManufacturerDTO>> updateManufacturer(@RequestBody ManufacturerDTO request) {
+        return manufacturerService.updateManufacturer(request);
+    }
+
+    @DeleteMapping(value = "/{code}")
+    public ResponseEntity<ResponseDTO<ManufacturerDTO>> deleteManufacturer(@PathVariable String code) {
+        return manufacturerService.deleteManufacturer(code);
+    }
 }
