d5f8cb9e025b88272bae48708cfb91e204fe4c64
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-08-04 09:22:59 +0300

    Ticket request

---
+++ tickets/src/main/java/org/sergei/tickets/rest/dto/TicketRequestDTO.java
@@ -0,0 +1,20 @@
+package org.sergei.tickets.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * @author Sergei Visotsky
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketRequestDTO implements Serializable {
+    private static final long serialVersionUID = -5198283871926597190L;
+    private Long passengerId;
+}
