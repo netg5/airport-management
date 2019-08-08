1dd930948c7a60ac0176301d3b38ccc255bf1c40
Sergei Visotsky <sergejs.visockis@emergn.com>
2019-08-08 17:32:46 +0300

    Swagger config modification

--- reports/src/main/java/org/sergei/reports/config/SwaggerConfig.java
+++ reports/src/main/java/org/sergei/reports/config/SwaggerConfig.java
@@ -35,7 +35,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("org.sergei.reports.rest.controller"))
+                .apis(RequestHandlerSelectors.basePackage("org.sergei.reports.rest"))
                 .paths(PathSelectors.any())
                 .build();
     }
