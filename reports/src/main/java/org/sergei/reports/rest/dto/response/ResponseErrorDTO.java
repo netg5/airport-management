6c6aa766075be0d4db299869ed296f8dbd3aaee0
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-08-18 15:11:11 +0300

    Reports initial commit

---
+++ reports/src/main/java/org/sergei/reports/rest/dto/response/ResponseErrorDTO.java
@@ -0,0 +1,22 @@
+package org.sergei.reports.rest.dto.response;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * @author Sergei Visotsky
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseErrorDTO implements Serializable {
+
+    private static final long serialVersionUID = 7356941682270970714L;
+
+    private String errorCode;
+    private String errorDescription;
+    private String errorType;
+}
---
+++ reports/src/main/java/org/sergei/reports/rest/exceptions/FlightRuntimeException.java
@@ -0,0 +1,27 @@
+package org.sergei.reports.rest.exceptions;
+
+/**
+ * @author Sergei Visotsky
+ */
+public class FlightRuntimeException extends RuntimeException {
+
+    public FlightRuntimeException() {
+        super();
+    }
+
+    public FlightRuntimeException(String message) {
+        super(message);
+    }
+
+    public FlightRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FlightRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    protected FlightRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
