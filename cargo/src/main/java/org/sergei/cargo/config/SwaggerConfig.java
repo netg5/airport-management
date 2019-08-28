4b642b6e1bc431a67ed73ba5cd48dc2738269171
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-08-28 21:54:22 +0300

    Swagger

--- cargo/src/main/java/org/sergei/cargo/config/SwaggerConfig.java
+++ cargo/src/main/java/org/sergei/cargo/config/SwaggerConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 /**
  * @author Sergei Visotsky
@@ -21,6 +24,19 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.sergei.cargo.rest"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiMetaData());
+    }
+
+    /**
+     * API information
+     *
+     * @return ApiInfo object
+     */
+    private ApiInfo apiMetaData() {
+        return new ApiInfo(
+                "Cargo flight management service",
+                "Cargo movement management service, receive, validate and proceed",
+                "1.0", null, null, null, null, List.of());
     }
 }
