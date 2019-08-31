c5b35ac9abf0b48b2cbe7233bc223a5d978e01de
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-08-31 08:09:40 +0300

    Minor changes

--- new-ui/src/app/App.js
+++ new-ui/src/app/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Router} from "react-router-dom";
 import './App.css';
-import Header from "../header/header";
+import Header from "../header/Header";
 
 class App extends Component {
     render() {
