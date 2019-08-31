f624373a000f539f036b0390bff27cecbc2e2b8f
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-08-31 11:28:09 +0300

    Minor refactoring

--- new-ui/src/app/App.js
+++ new-ui/src/app/App.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 import {Router} from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Header from "../header/Header";
 
 class App extends Component {
--- new-ui/src/index.js
+++ new-ui/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './bootstrap/bootstrap-grid.min.css'
-import './bootstrap/bootstrap-reboot.min.css'
-import './bootstrap/bootstrap.min.css'
-import './index.css';
-import App from './app/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./bootstrap/bootstrap-grid.min.css"
+import "./bootstrap/bootstrap-reboot.min.css"
+import "./bootstrap/bootstrap.min.css"
+import "./index.css";
+import App from "./app/App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
