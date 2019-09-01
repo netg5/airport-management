9e9bc812bceb6cb53732332e4e3613954614d733
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-09-01 10:42:33 +0300

    Template solution of timetable service

---
+++ timetable/src/main/java/org/sergei/timetable/config/security/ResourceServerConfig.java
@@ -0,0 +1,17 @@
+package org.sergei.timetable.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * Enables a Spring Security that authenticates requests via an incoming OAuth2 token
+ *
+ * @author Sergei Visotsky
+ */
+@Configuration
+@EnableResourceServer
+@Profile({"!plain", "!test"})
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+}
