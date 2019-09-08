4a3a547d8e7ea9af7d903dd8e454accc402c5ff5
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-09-08 08:25:23 +0300

    Fetching first endpoint

--- main-ui/src/home/Home.js
+++ main-ui/src/home/Home.js
@@ -1,82 +1,43 @@
 import React, {Component} from "react";
 
 class Home extends Component {
+    flights = {
+        flight: []
+    };
+    componentDidMount() {
+        fetch("http://localhost:8080/manager/getAllFlights")
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({flight: data})
+            })
+            .catch();
+    }
     render() {
         return (
             <div className="container">
                 <div className="card-deck mb-3 text-center">
-                    <div className="card mb-4 shadow-sm">
-                        <div className="card-header">
-                            <h4 className="my-0 font-weight-normal">Free</h4>
-                        </div>
-                        <div className="card-body">
-                            <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/ mo</small>
-                            </h1>
-                            <ul className="list-unstyled mt-3 mb-4">
-                                <li>10 users included</li>
-                                <li>2 GB of storage</li>
-                                <li>Email support</li>
-                                <li>Help center access</li>
-                            </ul>
-                            <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up for
-                                free
-                            </button>
-                        </div>
-                    </div>
-                    <div className="card mb-4 shadow-sm">
-                        <div className="card-header">
-                            <h4 className="my-0 font-weight-normal">Free</h4>
-                        </div>
-                        <div className="card-body">
-                            <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/ mo</small>
-                            </h1>
-                            <ul className="list-unstyled mt-3 mb-4">
-                                <li>10 users included</li>
-                                <li>2 GB of storage</li>
-                                <li>Email support</li>
-                                <li>Help center access</li>
-                            </ul>
-                            <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up for
-                                free
-                            </button>
-                        </div>
-                    </div>
-                    <div className="card mb-4 shadow-sm">
-                        <div className="card-header">
-                            <h4 className="my-0 font-weight-normal">Free</h4>
-                        </div>
-                        <div className="card-body">
-                            <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/ mo</small>
-                            </h1>
-                            <ul className="list-unstyled mt-3 mb-4">
-                                <li>10 users included</li>
-                                <li>2 GB of storage</li>
-                                <li>Email support</li>
-                                <li>Help center access</li>
-                            </ul>
-                            <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up for
-                                free
-                            </button>
-                        </div>
-                    </div>
-                    <div className="card mb-4 shadow-sm">
-                        <div className="card-header">
-                            <h4 className="my-0 font-weight-normal">Free</h4>
-                        </div>
-                        <div className="card-body">
-                            <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/ mo</small>
-                            </h1>
-                            <ul className="list-unstyled mt-3 mb-4">
-                                <li>10 users included</li>
-                                <li>2 GB of storage</li>
-                                <li>Email support</li>
-                                <li>Help center access</li>
-                            </ul>
-                            <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up for
-                                free
-                            </button>
-                        </div>
-                    </div>
+                    {this.flights.flight.map(
+                        (flight) => (
+                            <div className="card mb-4 shadow-sm">
+                                <div className="card-header">
+                                    <h4 className="my-0 font-weight-normal">Free</h4>
+                                </div>
+                                <div className="card-body">
+                                    <h1 className="card-title pricing-card-title">${flight.price}</h1>
+                                    <ul className="list-unstyled mt-3 mb-4">
+                                        <li>10 users included</li>
+                                        <li>2 GB of storage</li>
+                                        <li>Email support</li>
+                                        <li>Help center access</li>
+                                    </ul>
+                                    <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up
+                                        for
+                                        free
+                                    </button>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         )
