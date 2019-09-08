600fa0832c8fc4fbcd604117fa6e8b35c993e5a2
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-09-08 11:01:45 +0300

    renaming

--- main-ui/src/home/Home.js
+++ main-ui/src/home/Home.js
@@ -2,28 +2,35 @@ import React, {Component} from "react";
 
 class Home extends Component {
     flights = {
-        flight: []
+        response: []
     };
+
     componentDidMount() {
         fetch("http://localhost:8080/manager/getAllFlights")
             .then(res => res.json())
             .then((data) => {
-                this.setState({flight: data})
+                this.setState({
+                    response: data.map(item => ({
+                            price: item.response.price
+                        }
+                    ))
+                })
             })
             .catch();
     }
+
     render() {
         return (
             <div className="container">
                 <div className="card-deck mb-3 text-center">
-                    {this.flights.flight.map(
+                    {this.flights.response.map(
                         (flight) => (
                             <div className="card mb-4 shadow-sm">
                                 <div className="card-header">
                                     <h4 className="my-0 font-weight-normal">Free</h4>
                                 </div>
                                 <div className="card-body">
-                                    <h1 className="card-title pricing-card-title">${flight.price}</h1>
+                                    <h1 className="card-title pricing-card-title">${flight.response.price}</h1>
                                     <ul className="list-unstyled mt-3 mb-4">
                                         <li>10 users included</li>
                                         <li>2 GB of storage</li>
