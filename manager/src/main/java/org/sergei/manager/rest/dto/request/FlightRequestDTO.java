0892f74cbe40fb35b72146430514fe836794472d
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-09-11 08:16:57 +0300

    LocalDateTime Jackson format

--- manager/src/main/java/org/sergei/manager/rest/dto/request/FlightRequestDTO.java
+++ manager/src/main/java/org/sergei/manager/rest/dto/request/FlightRequestDTO.java
@@ -1,5 +1,6 @@
 package org.sergei.manager.rest.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,7 +19,11 @@ public class FlightRequestDTO implements Serializable {
     private static final long serialVersionUID = 5456566022748738538L;
     private Long routeId;
     private Double distance;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime departureTime;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime arrivalTime;
     private BigDecimal price;
     private String place;
