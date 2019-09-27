3ae0acadb85bd2ff0e62b353c9d52647bd0ae101
Sergei Visotsky <sergejs.visockis@emergn.com>
2019-09-27 18:32:54 +0300

    Header

--- main-ui-ng/src/app/app.component.ts
+++ main-ui-ng/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../bootstrap/bootstrap.min.css', 'app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'main-ui-ng';
+
+  ngOnInit(): void {
+    window.document.title = this.title;
+  }
 }
