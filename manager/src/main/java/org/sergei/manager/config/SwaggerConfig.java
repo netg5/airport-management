cb4948d9dd7dc64148e2c44bb70bef497d98319f
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-10-20 08:17:04 +0300

    Swagger apiInfo added

--- manager/src/main/java/org/sergei/manager/config/SwaggerConfig.java
+++ manager/src/main/java/org/sergei/manager/config/SwaggerConfig.java
@@ -18,8 +18,10 @@ package org.sergei.manager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -37,6 +39,14 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.sergei.manager.rest"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    @Bean
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Manager service API documentation")
                 .build();
     }
 }
--- orchestration/src/main/java/org/sergei/orchestration/config/AppConfig.java
+++ orchestration/src/main/java/org/sergei/orchestration/config/AppConfig.java
@@ -2,8 +2,10 @@ package org.sergei.orchestration.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,6 +23,14 @@ public class AppConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.sergei.orchestration.rest"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    @Bean
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Orchestration service API documentation")
                 .build();
     }
 }
