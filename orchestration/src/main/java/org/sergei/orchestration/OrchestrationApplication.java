0bce22df7a1ec5b733b8a9ff04db67a5975bee94
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-10-20 08:05:46 +0300

    @EnableFeignClients

--- orchestration/src/main/java/org/sergei/orchestration/OrchestrationApplication.java
+++ orchestration/src/main/java/org/sergei/orchestration/OrchestrationApplication.java
@@ -3,8 +3,10 @@ package org.sergei.orchestration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class OrchestrationApplication {
 
